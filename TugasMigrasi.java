284e4c54b9e5bd8b830dc097dfeb679d8f644088
Izzan Itsna <nafyren@gmail.com>
2024-08-22 09:43:14 +0700

    Add files via upload

---
+++ TugasMigrasi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbotugas;
+
+/**
+ *
+ * @author USER
+ */
+public class TugasMigrasi {
+    public static void main (String[] args){
+        String a = "Pemrograman Objek";
+        String b = "Pemrograman Terstruktur";
+        
+        if(a == b){
+            System.out.println("Mereka berdua sama");
+        }else{
+            System.out.println("Mereka berdua berbeda");
+        }
+    }
+}
